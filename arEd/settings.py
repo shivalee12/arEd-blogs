7fd48ac75c556edd8ef097982597809b81cfeb73
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-04-19 18:23:29 +0530

    Update settings.py

--- arEd/settings.py
+++ arEd/settings.py
@@ -195,7 +195,15 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 load_dotenv()
 SITE_ID = os.getenv('SITE_ID')
