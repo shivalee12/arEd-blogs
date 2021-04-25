d1fe65c0418dbbeb12b75f97ed164248d70231a2
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-04-25 15:49:25 +0530

    Update settings.py

--- arEd/settings.py
+++ arEd/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -195,15 +195,7 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 load_dotenv()
 SITE_ID = 2
